97724518067911347cb39e383068f82d729bb24f
moonlightlizaa <eliza.lacroes@gmail.com>
2023-02-16 20:36:17 +0100

    1

--- index.js
+++ index.js
@@ -2,26 +2,27 @@
 import express from "express";
 
 // API
+const url = "https://whois.fdnd.nl/api/v1/squad/";
 
-const url = "https://whois.fdnd.nl/api/v1/member/eliza-lacroes";
-const data = await fetch(url).then((response) => response.json());
-
-console.log(data);
-
-// Maak een nieuwe express app aan
+// Maak een nieuwe express app
 const app = express();
 
-// Stel ejs in als template engine en geef de 'views' map door
+// Stel in hoe we express gebruiken
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
-// Gebruik de map 'public' voor statische resources
 app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get("/", function (req, res) {
-  // res.send('Hello World!')
-  res.render("index", data);
+app.get("/", (request, response) => {
+  console.log(request.query.squad);
+
+  let slug = request.query.squad || "squad-a-2022";
+  let orderBy = request.query.orderBy || "name";
+  let squadUrl = url + slug + "?orderBy=" + orderBy + "&direction=ASC";
+
+  fetchJson(squadUrl).then((data) => {
+    response.render("index", data);
+  });
 });
 
 // Stel het poortnummer in waar express op gaat luisteren
@@ -32,3 +33,14 @@ app.listen(app.get("port"), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get("port")}`);
 });
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
